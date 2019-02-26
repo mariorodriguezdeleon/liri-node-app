186116d7f96495572d7b5f72cc2a9862f3d8c901
Mario Rodriguez <mrodrigu@northseattle.edu>
2019-02-26 08:15:15 -0800

    APIs are working, formatting needs to be completed

--- javascript/miri.js
+++ javascript/miri.js
@@ -1,9 +1,20 @@
+//GENERAL TODO;
+// 1. Output data to log.txt using the 'fs' module
+// 2. Do not over-write data in the log.txt file
+// 3. Use chalk to color your console output
+// 4. ????
+
 require("dotenv").config();
+
+// CONSTANTS =====================================================
 const fs = require('fs');
 const axios = require('axios');
+const moment = require('moment');
 const chalk = require('chalk');
 const keys = require("./keys.js");
 const Spotify = require('node-spotify-api');
+
+// GLOBAL VARIABLES ==============================================
 
 //init spotify object
 let spotify = new Spotify(keys.spot);
@@ -12,35 +23,45 @@ let spotify = new Spotify(keys.spot);
 let miriCommand = process.argv[2];
 let parameter = process.argv[3];
 
-
-console.log(parameter);
-
+// MIRI LOGIC
 switch (miriCommand) {
     case 'concert-this':
-        console.log(parameter);
-        // concertThis(parameter); //NOT WORKING YET
+        // console.log(parameter);
+        concertThis(parameter); //Working, but needs formatting, will use chalk after Aaron mentioned it
         break;
     case 'spotify-this-song':
         // console.log(parameter);
         spotifySong(parameter); //working, but needs formatting, will use chalk
         break;
     case 'movie-this':
-        console.log(parameter);
-        movieThis(parameter); // working, but needs formatting, will use chalk after Aaron mentioned it
+        // console.log(parameter);
+        movieThis(parameter); // working, but needs formatting, will use chalk 
         break;
     case 'do-what-it-says':
-        console.log(parameter); //NOT STARTED
+        console.log(parameter); //NOT COMPLETED
         break;
     default:
         console.log('You have to give a command');
 }
 
+
+// FUNCTIONS ====================================================
+
 function concertThis(parameter) {
-  //TODO: Add API call to the 'Bands in Town' peeps and display the returned results
+  //TODO: Format output
+  let queryUrl = "https://rest.bandsintown.com/artists/" + parameter + "/events?app_id=codingbootcamp";
+
+  axios({
+    method: 'get',
+    url: queryUrl
+  }).then(function(res){
+    console.log(res);
+  })
+  
 }
  
 function spotifySong(parameter) {
-  
+  //TODO: Format output
   let searchSong = '';
 
     if(parameter === undefined) {
@@ -55,7 +76,7 @@ function spotifySong(parameter) {
     limit: 1
   }).then(function(response) {
     console.log(response.tracks.items[0].artists[0].name);
-    console.log(response.tracks.)
+    console.log(response);
   })
   .catch(function(err){
     console.log(err);
@@ -64,6 +85,7 @@ function spotifySong(parameter) {
 }
 
 function movieThis(parameter) {
+  //TODO: Format output
   let findMovie = '';
 
   if (parameter === undefined) {
@@ -72,7 +94,7 @@ function movieThis(parameter) {
     findMovie = parameter
   }
 
-  var queryUrl = "http://www.omdbapi.com/?t=" + findMovie + "&y=&plot=short&apikey=6a93e976"
+  let queryUrl = "http://www.omdbapi.com/?t=" + findMovie + "&y=&plot=short&apikey=6a93e976"
 
   axios({
     method: 'get',
@@ -84,4 +106,15 @@ function movieThis(parameter) {
 
 function doWhatItSays(parameter) {
   //TODO: Add logic to read the random text document and have Miri do what is says.
+}
+
+function bandsintownOutput(response) {
+  //TODO: format the output of the returned response from spotify
+}
+
+function spotifyOutput(response) {
+  //TODO: format the output of the returned response from spotify
+}
+function omdbOutput(response) {
+  //TODO: format the output of the returned response from omdb
 }
